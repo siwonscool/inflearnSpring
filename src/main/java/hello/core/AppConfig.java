54d04c57799f8a3573dd192d6d053f7b86a1deaf
김시원 <kimsea13@naver.com>
2022-01-18 11:36:49 +0900

    OCP, DIP 원칙을 준수한 AppConfig 리팩터링

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,31 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService(){
+        return new MemberServiceImpl(this.memberRepository());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(
+                this.memberRepository()
+                ,this.discountPolicy()
+        );
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+}
---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,41 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 는 10% 할인 적용")
+    void vip_o(){
+        //given
+        Member member = new Member(1L,"memberVIP", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member,10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("일반고객은 10% 할인적용x")
+    void vip_x(){
+        //given
+        Member member = new Member(1L,"member",Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member,10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+
+}
